27ee556d9e09d8f402f42f08f9a18c48cf779aae
JuanJC199 <juancarranza96@outlook.com>
2020-11-15 23:23:12 -0600

    filtros

--- src/app/listar/listar.component.ts
+++ src/app/listar/listar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { allcontacts } from '../mocks/contacts';
 import { Contact } from '../models/contact';
 
 @Component({
@@ -13,11 +14,8 @@ export class ListarComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    this.contacts = [
-      {name: 'Juan', email: 'juan.carranza@gmail.com', telephone: '99812285', title: 'Sr' },
-      {name: 'Eduardo', email: 'eduardo.zepeda@gmail.com', telephone: '951468712', title: 'Sr' },
-      {name: 'Carlos', email: 'carlos.varela@gmail.com', telephone: '32654891', title: 'Sr' }
-    ]
+    this.contacts = allcontacts
+
   }
 
   onClick(contact: Contact):void {
